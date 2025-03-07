40984beee8a2ee3bd76bd00aefcbdf217d5bbcad
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-03-07 13:15:27 +0530

    dynamic port map

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => res.send("Hello,Multi-Cloud CI/CD!"));
-app.listen(3000, () => console.log("App running on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App running on port ${port}`));
