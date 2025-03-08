0090ea3cc8b5df77581fc95bca03623bd54f2e53
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-03-08 14:52:34 +0530

    Add test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.get('/', (req, res) => res.send('Hello, Updated Multi-Cloud CI/CD!'));
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App running on port ${port}`));
+
+app.get("/", (req, res) => res.send("Hello,Multi-Cloud CI/CD!"));
+
+if (require.main === module) {
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => console.log(`App running on port ${port}`));
+}
+
+module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../index");
+
+describe("GET /", () => {
+	it('should respond with "Hello,Multi-Cloud CI/CD!"', async () => {
+		const res = await request(app).get("/");
+		expect(res.statusCode).toEqual(200);
+		expect(res.text).toBe("Hello,Multi-Cloud CI/CD!");
+	});
+});
