6e5e3fe7ed96730f614886bc8afb78d885c693d3
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-03-08 16:08:19 +0530

    Sonar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello,Multi-Cloud CI/CD!'));
+app.get('/', (req, res) => res.send('Hello, Updated Multi-Cloud CI/CD!'));
 
 if (require.main === module) {
   const port = process.env.PORT || 3000;
