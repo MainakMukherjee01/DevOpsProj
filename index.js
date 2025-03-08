5905ccdb5324ebe68474fa4777ddd1d6b6bb826d
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-03-09 02:02:19 +0530

    Test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, Updated Multi-Cloud CI/CD! V final'));
+app.get('/', (req, res) => res.send('Hello, Updated Multi-Cloud CI/CD!'));
 
 app.get('/health', (req, res) => {
   const healthCheck = {
