44f1df7ab0707463eb2ceec99e54e5f387961f63
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-03-13 22:06:56 +0530

    PUSH to Reflect Change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, Multi-Cloud CI/CD!'));
+app.get('/', (req, res) => res.send('Hello, This is new Multi-Cloud CI/CD!'));
 
 app.get('/health', (req, res) => {
   const healthCheck = {
